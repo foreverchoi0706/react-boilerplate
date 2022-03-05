7a6b7286d14cb6c2a32eaf1162b176f6162f73bc
foreverchoi <57241273+foreverchoi0706@users.noreply.github.com>
2022-03-05 15:42:45 +0900

    Delete src/components/organisms directory

--- src/components/organisms/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { memo } from "react";
-import * as S from "./styled";
-
-const Footer = () => {
-  return <S.Footer>Footer</S.Footer>;
-};
-
-export default memo(Footer);
--- src/components/organisms/Footer/styled.ts
+++
@@ -1,9 +0,0 @@
-import styled, { css } from "styled-components";
-
-export const Footer = styled.footer(
-  () => css`
-    background-color: lightgray;
-    max-width: 1200px;
-    margin: 0 auto;
-  `
-);
