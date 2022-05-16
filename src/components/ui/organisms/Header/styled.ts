583ae85d56a9194163ec7c76ae241e22f740e6c9
foreverchoi <foreverchoi0706@gmail.com>
2022-05-16 22:49:53 +0900

    test

--- src/components/ui/organisms/Header/styled.ts
+++ src/components/ui/organisms/Header/styled.ts
@@ -12,7 +12,34 @@ const Styled = {
     () => css`
     display: flex;
     justify-content: space-between;
+  `),
+  Filter: styled.div(() => css`
+      background-color: white;
+      border-radius: 5px;
+    
+      width: 768px;
+      height : calc(100vh - 120px);
+      max-height : calc(100vh - 120px);
+  `),
+  FilterHeader: styled.header(() => css`
+    height: 73px;
+    border-bottom: 1px solid red;
+  `),
+  Section: styled.section(() => css`
+    overflow-y : auto;
+    height :  calc(100vh - 120px - 146px);
+    max-height: calc(100vh - 120px - 146px);
+  `),
+  BtnContainer: styled.div(() => css`
+   border-top: 1px solid red;
+    display: flex;
+    height: 73px;
+    gap: 8px;
+     > button{
+       flex-grow: 1;
+     }
   `)
+
 }
 
 export default Styled;
